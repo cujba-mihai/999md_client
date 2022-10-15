2c2cfc698219e521c4c12bf74e4efddc83b91c85
Mihai <mihai.cujba@indrivo.com>
2022-10-15 18:30:15 +0300

    Added default layout to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,29 @@
 import '../styles/global.scss';
 
 import type { AppProps } from 'next/app';
+import { NextPage } from 'next';
+import { ReactElement, ReactNode } from 'react';
+import MainLayout from '@/components/layout/_MainLayout';
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
-);
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+  withLayout: boolean | undefined;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+const MyApp = ({ Component, pageProps }: AppPropsWithLayout) => {
+  if (Component?.withLayout) {
+    Component.getLayout = function (page: ReactElement) {
+      return <MainLayout>{page}</MainLayout>;
+    };
+  }
+
+  const getLayout = Component.getLayout || ((page) => page);
+
+  return getLayout(<Component {...pageProps} />);
+};
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,7 @@
-import PageLayout from '@/components/page-layout/PageLayout';
-import Header from '@/components/header/Header';
-import { useState } from 'react';
-
 const Index = () => {
-  const [showSearchResults, setShowSearchResults] = useState(false);
-
-  const toggleSearchResults = () => {
-    setShowSearchResults((show) => !show);
-  };
-
-  return (
-    <PageLayout>
-      <Header
-        toggleSearchResults={toggleSearchResults}
-        showSearchResults={showSearchResults}
-        setShowSearchResults={setShowSearchResults}
-      />
-    </PageLayout>
-  );
+  return <h1>CHILD</h1>;
 };
+
+Index.withLayout = true;
 
 export default Index;
