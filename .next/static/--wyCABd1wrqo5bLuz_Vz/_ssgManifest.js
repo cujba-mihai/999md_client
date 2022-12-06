422d015e0e9b0ebf1d1801c46219e4a2323cc042
cujba-mihai <stereo12@mail.ru>
2022-12-06 22:15:51 +0200

    fix

--- .next/static/--wyCABd1wrqo5bLuz_Vz/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F[category]","\u002F[category]\u002F[subcategory]","\u002F[category]\u002F[subcategory]\u002Fproduct\u002F[id]","\u002Fproducts\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
